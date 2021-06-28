effcf2b1785923245826460e4efe0dc6cfe43f8a
MikeTutu <tutuanimichael@gmail.com>
2021-06-28 12:33:44 -0700

    Added 3-puts file

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,16 +1,16 @@
 #include "holberton.h"
 
 /**
- * _puts - prints a string
- * @str: value to be checked
- * Return: nothing
+ * _puts - prints a string to stdout
+ * @str: pointer to the string to print
  */
 void _puts(char *str)
 {
-	while (*str != '\0')
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		_putchar(*str);
-		str++;
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
