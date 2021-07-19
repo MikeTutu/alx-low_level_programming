4bdd5fd328e00f21a7dd95a0f4feb3f8bc163127
MikeTutu <tutuanimichael@gmail.com>
2021-07-19 12:35:17 -0700

    Added 3-function_like_macro file

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,15 @@
-#ifndef POS
-#define POS
-#define ABS(x) ((x) < 0 ? (-x) : (x))
-#endif
+#include <stdio.h>
+/**
+ * ABS - return absolute value of int
+ * @x: value passed
+ * Return: absolute value of x
+ */
+unsigned int ABS(int x)
+{
+	if (x < 0)
+	{
+		x = x * -1;
+	}
+	printf("%d\n", x);
+	return (0);
+}
