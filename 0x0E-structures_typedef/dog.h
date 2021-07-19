9e8d27758fac91ebe4a0ee81b9327306ee368d84
MikeTutu <tutuanimichael@gmail.com>
2021-07-19 12:58:33 -0700

    Added dog file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - defines type
+ * @name: dog name
+ * @age: age
+ * @owner: dogs owner
+ * Description: d
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+#endif
