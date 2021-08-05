77b5ac7604d363013e90a7d2cc306c354f95562d
MikeTutu <tutuanimichael@gmail.com>
2021-08-05 14:30:25 -0700

    Added set_bit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,20 +1,18 @@
-#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include "holberton.h"
+
 /**
- * set_bit - sets the value of a bit to 1 at given index
- * @n: number
- * @index: position
- * Return: 1 for success, -1 for failure
- */
+  * set_bit - Sets the value of a bit to 1 at a given index
+  * @n: The number to modify
+  * @index: The index in the number to modify
+  *
+  * Return: 1 if it worked, or -1 if an error occurred
+  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-  unsigned long int byte_size;
+	if (index > sizeof(unsigned long int) * 8)
+		return (-1);
 
-  byte_size = sizeof(unsigned long int) * 8;
-
-  if (index < byte_size)
-    {
-      *n = *n | (1 << index);
-      return (1);
-    }
-  return (-1);
+	return ((*n |= 1 << index) ? 1 : -1);
 }
